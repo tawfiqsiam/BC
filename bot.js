f28d08de96076224569bab4174371bf3a220ebd3
tawfiqsiam <toto.siam999@gmail.com>
2019-09-15 22:20:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -75,7 +75,7 @@ var prefix = "-";
                            if(!message.channel.guild) return;
                let args = message.content.split(' ').slice(1).join(' ');
                if (message.content.startsWith('-abc')){
-                if (message.author.id !== '239583032883937301ii') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+                if (message.author.id !== '239583032883937301') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
                message.channel.sendMessage('جار ارسال الرسالة |✅')
                client.users.forEach(m =>{
                m.sendMessage(args)
